e15ed17c3a56f8e8c8bd6d3708f9d17213ba7bfb
Bruna <brunadasilvabz98@gmail.com>
2022-11-21 11:05:51 +0100

    add starter kit + migration

---
+++ src/components/App.js
@@ -0,0 +1,72 @@
+import "../styles/main.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page">
+        <header>
+          <h1 className="header__title">Juego del ahorcado</h1>
+        </header>
+        <main className="main">
+          <section>
+            <div className="solution">
+              <h2 className="title">Solución:</h2>
+              <ul className="letters">
+                <li className="letter">k</li>
+                <li className="letter">a</li>
+                <li className="letter"></li>
+                <li className="letter">a</li>
+                <li className="letter">k</li>
+                <li className="letter">r</li>
+                <li className="letter"></li>
+                <li className="letter">k</li>
+                <li className="letter">e</li>
+                <li className="letter">r</li>
+              </ul>
+            </div>
+            <div className="error">
+              <h2 className="title">Letras falladas:</h2>
+              <ul className="letters">
+                <li className="letter">f</li>
+                <li className="letter">q</li>
+                <li className="letter">h</li>
+                <li className="letter">p</li>
+                <li className="letter">x</li>
+              </ul>
+            </div>
+            <form className="form">
+              <label className="title" htmlFor="last-letter">
+                Escribe una letra:
+              </label>
+              <input
+                autoComplete="off"
+                className="form__input"
+                maxLength="1"
+                type="text"
+                name="last-letter"
+                id="last-letter"
+              />
+            </form>
+          </section>
+          <section className="dummy error-5">
+            <span className="error-13 eye"></span>
+            <span className="error-12 eye"></span>
+            <span className="error-11 line"></span>
+            <span className="error-10 line"></span>
+            <span className="error-9 line"></span>
+            <span className="error-8 line"></span>
+            <span className="error-7 line"></span>
+            <span className="error-6 head"></span>
+            <span className="error-5 line"></span>
+            <span className="error-4 line"></span>
+            <span className="error-3 line"></span>
+            <span className="error-2 line"></span>
+            <span className="error-1 line"></span>
+          </section>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
